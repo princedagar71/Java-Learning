f8e7b2a4f82e17e3becc15b1d99e584ec4c5e3ed
Prince <princedagar71@gmail.com>
2021-07-15 20:57:37 +0530

    "Conditional Operators Complete"

--- src/com/company/4.Conditional Operators.java
+++ src/com/company/4.Conditional Operators.java
@@ -2,5 +2,18 @@ package com.company;
 class Conditional_Operators{
     public static void main(String[] args){
         //> < >= <= == !=
+        int a=10,b=5;
+        if(a>b)
+            System.out.println(true);
+        if (a<b)
+            System.out.println(true);
+        if (a>=b)
+            System.out.println(true);
+        if (a<=b)
+            System.out.println(true);
+        if (a==b)
+            System.out.println(true);
+        if (a!=b)
+            System.out.println(true);
     }
 }
